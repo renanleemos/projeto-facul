c1abc5afc27d225a74af216b0ff416e7f4ee795c
Renan Lemos <renanlemos1612@gmail.com>
2023-12-04 13:18:34 -0300

    Finalizado layout

---
+++ js/swiper.js
@@ -0,0 +1,41 @@
+// Swiper
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
+
+// Menu open Close
+let menu = document.querySelector(".menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menu.onclick = () => {
+  menu.classList.toggle("move");
+  navbar.classList.toggle("open-menu");
+};
+
+// Close Menu on Scroll
+window.onscroll = () => {
+  menu.classList.remove("move");
+  navbar.classList.remove("open-menu");
+};
+
+// ScrollReveal ANIMATION
+const animate = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: "2500",
+  delay: "400",
+});
+
+animate.reveal(".nav");
+animate.reveal(".home-text", { origin: "left" });
+animate.reveal(".home-img", { origin: "bottom" });
+animate.reveal(".ser-box, .product-box, .team-box, .footer", { interval: 100 });
