1bce996d12b83e0e96704feb0a6221ebc785f5c1
Renan Lemos <renanlemos1612@gmail.com>
2023-12-04 16:53:46 -0300

    criando servidor de pagamento

--- js/main.js
+++ js/main.js
@@ -1,63 +1,3 @@
-// // Products Array
-// let products = [
-//   {
-//     id: 1,
-//     title: "Café com Leite",
-//     price: 7.5,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjOv4xncELe0Z1RFVrgL3CWKn-T_SopTHdp_Wjg7Fmtk5OrkHuVs3CjLgj1Hrk8ke3uXFyks681B5-H7mgN1Z-eVJg2AUA5Ak_5EP6tA45ocpoMvuTju3_8flSzVj_Hjl5jaXXudUwYGKzSuM329KOoaV1erc3fB58QD3mnYaaJ-TeRgggjkiDCzd5x0Q0/s320/product-1.webp",
-//   },
-//   {
-//     id: 2,
-//     title: "Croissant de Queijo",
-//     price: 20,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEj1rUonD8su7jhNYlWOitAZPM13dTpQxGq8gzqT1y9rA56oCYgljrfAbnGZt-9WRIlBMd6pEpMgK-DgyXFwXF5QW5b3n2ePrMvPiOCxtzWXkIH02LY994F-RMKsoGrbKYn3RX8q9vyK0c9DoT9mi3hWnZycYWPKflEFXixYjMmEpHVYE5KohKaVXdhyBKc/s320/product-2.jpg",
-//   },
-//   {
-//     id: 3,
-//     title: "Pão de Mel",
-//     price: 8.75,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEiSXhhGid_fCa0X0aC_0nscPxceIXYAdVoOVq3F3G0uWGwNffjOCxqi-38UUzPkLC5oA04yc9jOOVTvB-XteYOJrb5vQRXEbdpn7L-F2kb5-KfzO8mOFTaQq83-4MWohdF2OUAWtv7YEaNqyl7oqtddxutCvmQh2yZcGr3qqJbS_5kCjKhCEkc9QuDbe1I/s320/product-3.webp",
-//   },
-//   {
-//     id: 4,
-//     title: "Hambúrguer de Carne",
-//     price: 25,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjTXZxyrh6jmPkmzpAJnk3RN3Q3EvyY1OvwNwgG7XohvmJKBjRYksKjdywIQVPcB4TGIoq411Ww_1lhayJi3GWKJLXSD8ed-JAefqT0PgiTCQbRJCsxwrqSM3Kv_bS8k-1WQMNqozr_jEePfDwAcusCbwuARRUOfjgWe7QF-7q8WrJ6FILuISL2VPlmFpg/s320/product-4.webp",
-//   },
-//   {
-//     id: 5,
-//     title: "Hambúrguer com ovo",
-//     price: 26.99,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjXnFIV6JLLazjHMk3REnASPQa7OfP9dsDVlvQwJVRo_EQDhlDApV0o7ZBLjeXxSNGKPVBd2cmUAVOrqMtpgIIYOrEGZlBZtYLlqjDaId9yd93cEdWxn0ss_vbpPXlmthbL7Fkr4sek0KtVlZxPGG93t79DwdjJ8wyBgV5cBvlw_iD_x1R79vEtHyqq-KU/s320/product-5.jpeg",
-//   },
-//   {
-//     id: 6,
-//     title: "Hot Dog com Fritas",
-//     price: 19,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh0FikhoDhRhDkAm6vpWNpBv0QRIJ5kRYuDAzwbeUYX-Cs4lJ0oRQYdAcJQkvIi5iWLUxzjNCIpJ7zfz_sHIm9SSgQT3MJqRlnTipY8h3JHiQVko4oBtB3g-HtUvomStXqVJQdPsguIz2WZNLfDavZELHrWlBM8gBoF7m18PWI6_H9pfF1pWuY8rhFDiNE/s320/product-6.webp",
-//   },
-//   {
-//     id: 7,
-//     title: "Petiscos",
-//     price: 12.99,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjvQr5DzWLvrD44acY-TzjK-_UsYVf-BoEfpRfsTB38OiE4OX3Er1EI_ykG-PNMhlUDB5chaekfgmHPxEgFVYlnHQzye5AocAepNW8N6zqjpzwHM6El2H-oIqcaScSNvDgIXRiNRTsYcbX7HHXu57t3dsAKp9oIJWp7sUoEZ32TTW0EH-FrZ3A-lJ4yI34/s320/product-7.webp",
-//   },
-//   {
-//     id: 8,
-//     title: "Fatia de Bolo",
-//     price: 14.99,
-//     image:
-//       "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhINlSn9Hx-meGsUFlTl9J-FofKGQ5iBKvRhgf7Leii4BqM9NAUDvdEL-WWxo-gWUON8YISUN5B8ZCuM4tnXX0AVkCpwkL9UsVqk2fAz5IYyobkfZIAt-ujsbulFoh304ymqkaIKp1bG91f4oV3otCHOBZBMfah6A0fsmwTupCxAdEayVN7I8xEND66NDo/s320/product-8.jpeg",
-//   },
-// ];
-
 // Cart Open Close
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
@@ -99,6 +39,7 @@ function ready() {
     var button = addCart[i];
     button.addEventListener("click", addCartClicked);
   }
+  loadCartItems();
 }
 
 //  Remove Cart Item
@@ -106,6 +47,7 @@ function removeCartItem(event) {
   var buttonClicked = event.target;
   buttonClicked.parentElement.remove();
   updateTotal();
+  saveCartItems();
 }
 
 // Quantity Change
@@ -115,17 +57,21 @@ function quantityChanged(event) {
     input.value = 1;
   }
   updateTotal();
+  saveCartItems();
+  updateCartIcon();
 }
 
 // add cart function
 function addCartClicked(event) {
   var button = event.target;
   var shopProducts = button.parentElement.parentElement;
-  var title = shopProducts.getElementsByClassName("product-title").innerText;
-  var price = shopProducts.getElementsByClassName("price").innerText;
+  var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+  var price = shopProducts.getElementsByClassName("price")[0].innerText;
   var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
   addProductToCart(title, price, productImg);
   updateTotal();
+  saveCartItems();
+  updateCartIcon();
 }
 
 function addProductToCart(title, price, productImg) {
@@ -161,6 +107,8 @@ function addProductToCart(title, price, productImg) {
   cartShopBox
     .getElementsByClassName("cart-quantity")[0]
     .addEventListener("change", quantityChanged);
+  saveCartItems();
+  updateCartIcon();
 }
 
 //  Update Total
@@ -179,4 +127,68 @@ function updateTotal() {
   // if price contain some cents
   total = Math.round(total * 100) / 100;
   document.getElementsByClassName("total-price")[0].innerText = "$" + total;
+  // save total to localstorage
+  localStorage.setItem("cartTotal", total);
+}
+
+// Keep item in cart when page refresh with localstorage
+function saveCartItems() {
+  var cartContent = document.getElementsByClassName("cart-content")[0];
+  var cartBoxes = cartContent.getElementsByClassName("cart-box");
+  var cartItems = [];
+
+  for (var i = 0; i < cartBoxes.length; i++) {
+    cartBox = cartBoxes[i];
+    var titleElement = cartBox.getElementsByClassName("cart-product-title")[0];
+    var priceElement = cart.getElementsByClassName("cart-price")[0];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    var productImg = cartBox.getElementsByClassName("cart-img")[0].src;
+
+    var item = {
+      title: titleElement.innerText,
+      price: priceElement.innerText,
+      quantity: quantityElement.value,
+      productImg: productImg,
+    };
+    cartItems.push(item);
+  }
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+}
+
+// Loads in cart
+function loadCartItems() {
+  var cartItems = localStorage.getItem("cartItems");
+  if (cartItems) {
+    cartItems = JSON.parse(cartItems);
+
+    for (var i = 0; i < cartItems.length; i++) {
+      var item = cartItems[i];
+      add.addProductToCart(item.title, item.price, item.productImg);
+
+      var cartBoxes = document.getElementsByClassName("cart-box");
+      var cartBox = cartBoxes[cartBoxes.length - 1];
+      var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+      quantityElement.value = item.quantity;
+    }
+  }
+  var cartTotal = localStorage.getItem("cartTotal");
+  if (cartTotal) {
+    document.getElementsByClassName("total-price")[0].innerText =
+      "$" + cartTotal;
+  }
+  updateCartIcon();
+}
+
+// quantity in cart icon
+function updateCartIcon() {
+  var cartBoxes = document.getElementsByClassName("cart-box");
+  var quantity = 0;
+
+  for (var i = 0; i < cartBoxes.length; i++) {
+    var cartBox = cartBoxes[i];
+    var quantityElement = cartBox.getElementsByClassName("cart-quantity")[0];
+    quantity += parseInt(quantityElement.value);
+  }
+  var cartIcon = document.querySelector("#cart-icon");
+  cartIcon.setAttribute("data-quantity", quantity);
 }
