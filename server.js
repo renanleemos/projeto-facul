fd9ebcdb38af547ab1b4eea3e51ef50bfd4f96ce
Renan Lemos <renanlemos1612@gmail.com>
2023-12-04 17:56:22 -0300

    projeto finalizado

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import stripe from "stripe";
 
 // Load variables
 dotenv.config();
@@ -7,14 +8,16 @@ dotenv.config();
 //  start server
 const app = express();
 
-app.use(express.static("projeto-facul"));
+app.use(express.static("public"));
 app.use(express.json());
-
 
 // home route
 app.get("/", (req, res) => {
-  res.sendFile('index.html', {root: 'projeto-facul'});
+  res.sendFile("index.html", { root: "public" });
 });
+
+stripe 
+let stripeGateway = stripe(process.env.stripe_api);
 
 app.listen(3000, () => {
   console.log("listening on port 3000;");
